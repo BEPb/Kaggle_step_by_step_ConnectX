3607ad74f954e429edcaef404f19f9834435b0f8
BEPb <andrej.marinchenko@gmail.com>
2022-02-08 21:55:53 +0300

    upd 1 steep

--- 01.step_01/02.intro.py
+++ 01.step_01/02.intro.py
@@ -10,7 +10,7 @@ Date: 2022-02-06
 """
 from kaggle_environments import make, evaluate
 
-# Создать игровую среду
+# Создать игровую среду симуляции ConnectX
 env = make("connectx", debug=True)  # среда connectX
 # env = make("tictactoe")  # среда крестики-нолики
 # env = make("identity") # Для отладки, действие является наградой
@@ -36,7 +36,8 @@ runTimeout       - Максимальное время выполнения (в 
 '''
 
 env = make("connectx", configuration={
-  "columns": 19,  # этот параметр специфический только для игры ConnectX.
+  "columns": 19,  # этот параметр специфический только для игры ConnectX и означает количество конок = 19,
+  # по умолчанию количество колонок равно 7.
   "actTimeout": 10,
 })
 # print(dir(env))  # выведет все обрабатываемые параметры
@@ -50,5 +51,9 @@ env.reset  # сбросить среду
 num_agents = 2
 reset_state = env.reset(num_agents)
 
-# Список доступных агентов по умолчанию
-print(list(env.agents))
+# В игровой среде ConnectX есть уже реализованные для вас агенты. Чтобы увидеть список этих агентов
+print(list(env.agents))  # Список доступных агентов по умолчанию 'random', 'negamax'
+
+env = make("connectx")
+env.run(["random", "random"])  # This line of code starts the game
+env.render(mode="ipython")  # # this line of code shows the game in our laptop, from this we can conclude that the game will take place even if this line of code does not exist
